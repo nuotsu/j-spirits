f63fcf3591bb8c8d40ba9aa3d8f7fe26d32309ca
Mitchell Christ <xzmaru@gmail.com>
2022-03-05 04:01:25 -0800

    add netlify adapter

--- packages/web/src/lib/form/NetlifyForm.svelte
+++ packages/web/src/lib/form/NetlifyForm.svelte
@@ -1,5 +1,5 @@
 <form {name} {...$$props} data-netlify="true" netlify-honeypot="bot-field">
-	<input type="hidden" name="form-name" value="contact" hidden>
+	<input type="hidden" name="form-name" value={name} hidden>
 	<input type="hidden" name="bot-field" hidden>
 
 	<slot></slot>
