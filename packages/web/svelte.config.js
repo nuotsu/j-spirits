c893122e4e562b7cfb0dfe4ffac2c077fc808eb7
Mitchell Christ <xzmaru@gmail.com>
2022-03-07 18:28:48 -0800

    localize entries

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,13 +1,21 @@
 import adapter from '@sveltejs/adapter-netlify'
 import path from 'path'
 
+const locales = ['en', 'ja']
+
+function localizeEntry(entry) {
+	return locales.map(locale => `/${ locale }${ entry }`)
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 
 		prerender: {
-			entries: ['/contact'],
+			entries: [
+				...localizeEntry('/contact')
+			],
 		},
 
 		vite: {
