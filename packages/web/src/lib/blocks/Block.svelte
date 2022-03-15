1d3c1b7264b58d8ba00aebb2c4f3c647e3912be7
Mitchell Christ <xzmaru@gmail.com>
2022-03-14 21:17:20 -0700

    Revert "try dynamic import"
    This reverts commit 02d11d242d0177c0bbdf5197554f35124083e4e2.

--- packages/web/src/lib/blocks/Block.svelte
+++ packages/web/src/lib/blocks/Block.svelte
@@ -1,46 +1,20 @@
-<!-- {#if _type === 'heroHome'} <HeroHome {...props} />
+{#if _type === 'heroHome'} <HeroHome {...props} />
 {:else if _type === 'newsList'} <NewsList {...props} />
 {:else if _type === 'gameListCompact'} <GameListCompact {...props} />
 {:else if _type === 'gameList'} <GameList {...props} />
 {:else if _type === 'upcomingGameList'} <UpcomingGameList {...props} />
 {:else if _type === 'roster'} <Roster {...props} />
 {:else if _type === 'contactForm'} <ContactForm {...props} />
-{/if} -->
-
-{#await getComponent()}
-	<pre>Loading...</pre>
-{:then Component}
-	<svelte:component this={Component} {...props} />
-{/await}
+{/if}
 
 <script>
-	// import HeroHome from '~/lib/blocks/HeroHome.svelte'
-	// import GameList from '~/lib/blocks/games/GameList.svelte'
-	// import GameListCompact from '~/lib/blocks/games/GameListCompact.svelte'
-	// import UpcomingGameList from '~/lib/blocks/games/UpcomingGameList.svelte'
-	// import NewsList from '~/lib/blocks/NewsList.svelte'
-	// import Roster from '~/lib/blocks/roster/Roster.svelte'
-	// import ContactForm from '~/lib/forms/ContactForm.svelte'
+	import HeroHome from '~/lib/blocks/HeroHome.svelte'
+	import GameList from '~/lib/blocks/games/GameList.svelte'
+	import GameListCompact from '~/lib/blocks/games/GameListCompact.svelte'
+	import UpcomingGameList from '~/lib/blocks/games/UpcomingGameList.svelte'
+	import NewsList from '~/lib/blocks/NewsList.svelte'
+	import Roster from '~/lib/blocks/roster/Roster.svelte'
+	import ContactForm from '~/lib/forms/ContactForm.svelte'
 
 	let { _type, ...props } = $$props
-
-	async function getComponent() {
-		switch (_type) {
-			case 'heroHome':
-				return (await import('~/lib/blocks/HeroHome.svelte')).default
-			case 'gameList':
-				return (await import('~/lib/blocks/games/GameList.svelte')).default
-			case 'gameListCompact':
-				return (await import('~/lib/blocks/games/GameListCompact.svelte')).default
-			case 'upcomingGameList':
-				return (await import('~/lib/blocks/games/UpcomingGameList.svelte')).default
-			case 'newsList':
-				return (await import('~/lib/blocks/NewsList.svelte')).default
-			case 'roster':
-				return (await import('~/lib/blocks/roster/Roster.svelte')).default
-			case 'contactForm':
-				return (await import('~/lib/forms/ContactForm.svelte')).default
-			default: return null
-		}
-	}
 </script>
