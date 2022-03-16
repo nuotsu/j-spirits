c567201921a1f5b00e6b7b1aebd04dd6fb9a6516
Mitchell Christ <xzmaru@gmail.com>
2022-03-16 00:01:26 -0700

    fix locale switcher

--- packages/nextjs/lib/LocaleSwitcher.jsx
+++ packages/nextjs/lib/LocaleSwitcher.jsx
@@ -1,22 +1,26 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { GlobalContext } from 'lib/store/Global'
 import { useRouter } from 'next/router'
 
 export default ({ className }) => {
 	const { locale, locales } = useContext(GlobalContext)
+	const [$l, set$l] = useState(locale)
 
 	const router = useRouter()
 	const { pathname, asPath, query } = router
 
 	function onChange({ target }) {
 		router.push({ pathname, query }, asPath, { locale: target.value })
-		console.log(locale)
 	}
+
+	useEffect(() => {
+		set$l(locale)
+	}, [set$l, locale])
 
 	return (
 		<select
 			className={className}
-			defaultValue={locale}
+			value={$l}
 			onChange={onChange}
 		>
 			{locales.map(({ value, label }, key) => (
