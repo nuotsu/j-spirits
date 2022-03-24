e6c6cf7a6eea91931f0b9fe0e6b26ea84bdaadc1
Mitchell Christ <xzmaru@gmail.com>
2022-03-24 00:14:35 -0700

    remove honeypot

--- packages/nextjs/lib/block/form/NetlifyForm.jsx
+++ packages/nextjs/lib/block/form/NetlifyForm.jsx
@@ -3,10 +3,11 @@ const Form = ({ name, className, children, ...props }) => (
 		name={name}
 		className={className}
 		{...props}
-		data-netlify="true" data-netlify-honeypot="bot-field"
+		data-netlify="true"
+		// data-netlify-honeypot="bot-field"
 	>
 		<input type="hidden" name="form-name" value={name} hidden />
-		<input type="hidden" name="bot-field" hidden />
+		{/* <input type="hidden" name="bot-field" hidden /> */}
 
 		{children}
 	</form>
